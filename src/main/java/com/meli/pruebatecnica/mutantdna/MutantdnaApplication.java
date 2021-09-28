b2fbf4dda81ea10caa95509b0c7b96c0658c890f
fepego-penag <cf.pena1138@uniandes.edu.co>
2021-09-28 07:24:33 -0500

    Project Initialization

---
+++ src/main/java/com/meli/pruebatecnica/mutantdna/MutantdnaApplication.java
@@ -0,0 +1,13 @@
+package com.meli.pruebatecnica.mutantdna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantdnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantdnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/pruebatecnica/mutantdna/MutantdnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.pruebatecnica.mutantdna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantdnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
