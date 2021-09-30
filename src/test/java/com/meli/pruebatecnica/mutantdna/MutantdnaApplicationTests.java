10e270c2eb0ac2f3dce61a8c0ad0d014968ffaf3
fepego-penag <cf.pena1138@uniandes.edu.co>
2021-09-29 23:02:30 -0500

    adjusting main test

--- src/test/java/com/meli/pruebatecnica/mutantdna/MutantdnaApplicationTests.java
+++ src/test/java/com/meli/pruebatecnica/mutantdna/MutantdnaApplicationTests.java
@@ -1,6 +1,7 @@
 package com.meli.pruebatecnica.mutantdna;
 
 import com.meli.pruebatecnica.mutantdna.rest.MutantController;
+import com.meli.pruebatecnica.mutantdna.service.DnaChainValidator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.main.lazy-initialization=true", classes = {DnaChainValidator.class})
 class MutantdnaApplicationTests {
 
 	@Test
